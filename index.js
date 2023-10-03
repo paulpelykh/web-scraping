665a8cbba05fd4b739944a1461461172a6d72276
Paul <eeddll1505@gmail.com>
2023-10-03 19:37:20 +0300

    scrapping text from HTML

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
-const puppeteer = require("puppeteer");
+const puppeteer = require('puppeteer');
+const fs = require('fs/promises');
 
 async function start() {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
+  await page.goto('https://learnwebcode.github.io/practice-requests/');
 
-  await page.goto("https://uk.wikipedia.org/wiki/React");
-  await page.screenshot({ path: "amazing.png", fullPage: true });
+  const names = await page.evaluate(() => {
+    return Array.from(document.querySelectorAll('.info strong')).map(
+      x => x.textContent
+    );
+  });
+  await fs.writeFile('names.txt', names.join('\r\n'));
+
   await browser.close();
 }
 
